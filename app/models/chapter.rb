7b3308c95c91dad2026a9f2cf163572273f65d52
gktttw <gktttwsam@gmail.com>
2024-07-13 15:40:44 +0800

    make it better

---
+++ app/models/chapter.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: chapters
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  course_id  :integer          not null
+#  ordering   :float            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Chapter < ActiveRecord::Base
+  validates :name, presence: true
+  validates :ordering, presence: true
+  validates :ordering, numericality: true
+
+  belongs_to :course
+  has_many :units, dependent: :delete_all
+  accepts_nested_attributes_for :units
+end
